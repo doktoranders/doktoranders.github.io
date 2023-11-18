99e19b33f7e55677192c7051b5510f662a489185
doktoranders <147780381+doktoranders@users.noreply.github.com>
2023-11-19 06:50:30 +0700

    Add files via upload

---
+++ def func(__kwargs) 2.py
@@ -0,0 +1,9 @@
+def full_name(**arguments):
+  print(arguments)
+
+name = input('Enter a name: ')
+age = input('Enter an age: ')
+name2 = input('Enter a name: ')
+age2 = input('Enter an age: ')
+
+full_name(name = name, age = age, name2 = name2, age2 = age2)
