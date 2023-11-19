d0e121153016cdc4f3c9ca6eb4710e1fc3e163d8
doktoranders <147780381+doktoranders@users.noreply.github.com>
2023-11-19 09:43:22 +0700

    Add files via upload

--- OOP8.py
+++ OOP8.py
@@ -1,16 +1,26 @@
 class Person:
-  def __init__(self):
-      person_name = input('Enter name: ')
-      person_age = int(input('Enter age: '))
+  def __init__(self, person_name, person_age):
       self.name = person_name
       self.age = person_age
 
   def display_info(self):
-      print(f'name: {self.name}')
-      print(f'age: {self.age}')
+      print(f'Name: {self.name}, Age: {self.age}')
 
-person1 = Person()
-person1.display_info()
+def create_person_list(num_people):
+  person_list = []
+  for _ in range(num_people):
+      name = input('Enter name: ')
+      age = int(input('Enter age: '))
+      person = Person(name, age)
+      person_list.append(person)
+  return person_list
 
-person2 = Person()
-person2.display_info()
+def main():
+  num_people = 3
+  people = create_person_list(num_people)
+  for person in people:
+      person.display_info()
+  return(people)
+
+if __name__ == "__main__":
+  main()
